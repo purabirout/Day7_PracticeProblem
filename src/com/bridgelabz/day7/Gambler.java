da587c3de3f6325ccca5411ba08fb34e3802b5d1
purabirout <routpurabi96@gmail.com>
2022-04-10 23:49:03 +0530

    Adding Day7 files

---
+++ src/com/bridgelabz/day7/Gambler.java
@@ -0,0 +1,55 @@
+package com.bridgelabz.day7;
+	import java.util.Random;
+	import java.util.Scanner;
+
+	public class Gambler {
+
+		/**
+		 * Function to simulate gamble
+		 * 
+		 * @param n      the value of no of times to bet
+		 * @param $stack the amount which you have
+		 * @param $goal  the value which to reach to win the gamble (not the bet)
+		 */
+		static void gamble(int noOfTimes, int $Stack, int $Goal) {
+			int bets = 0; // total number of bets made
+			int wins = 0; // total number of games won
+
+			// repeat number of times
+			for (int i = 0; i < noOfTimes; i++) {
+				int cash = $Stack;
+
+				while (cash > 0 && cash < $Goal) {
+					bets++;
+
+					if (Math.random() < 0.5)
+						cash++; // win $1
+					else
+						cash--; // lose $1
+				}
+				if (cash == $Goal)
+					wins++; // to find no. of wins
+			}
+
+			// print results
+			System.out.println();
+			System.out.println(wins + " wins out of of " + noOfTimes);
+			System.out.println("Percent of games won = " + 100.0 * wins / noOfTimes);
+			System.out.println("Percent of games lost = " + 100.0 * (noOfTimes - wins) / noOfTimes);
+			System.out.println("Total bets in " + noOfTimes + " games = " + bets);
+		}
+
+		public static void main(String[] args) {
+			Scanner s = new Scanner(System.in);
+			int n, $stack, $goal;
+			System.out.println("enter stack amount");
+			$stack = s.nextInt();
+			System.out.println("enter goal amount");
+			$goal = s.nextInt();
+			System.out.println("enter no of times to gamble");
+			n = s.nextInt();
+			s.close();
+			gamble(n, $stack, $goal);
+
+		}
+	}
